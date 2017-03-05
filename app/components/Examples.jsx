91eda8ccf8457b7a4b4553856283cdfc16d231e0
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-05 14:09:47 -0800

    center text and add a few unwired up examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,20 @@
 var React = require('react');
-
-var Examples = () => <h2>Examples Component</h2>
+var {Link} = require('react-router')
+var Examples = () => {
+  return (
+    <div>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
+  )
+}
 
 module.exports = Examples;
