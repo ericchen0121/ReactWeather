229d2410e522f30f56a4c624e3c7ecd7cc6bc5ae
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-05 14:27:55 -0800

    Added links to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,18 @@ var React = require('react');
 
 // stateless functional components
 var About = () => {
-  return <h2>About Component!</h2> 
+  return (
+    <div>
+      <h1 className='text-center  '>About</h1>
+      <p>This little app was created with OpenWeatherMap's API and React.</p>
+      <ul>
+        <li>Check out the <a href='http://openweathermap.org/'>OpenWeatherMap</a> page.</li>
+        <li>And Facebook's <a href='https://facebook.github.io/react/'>React</a> library for front-end development.</li>
+        <li>Check out the <a href='https://github.com/ericchen0121/ReactWeather'>Github code</a> for this app.</li>
+      </ul>
+    </div>
+
+  )
 }
 
 module.exports = About;
