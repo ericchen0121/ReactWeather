e172dbfebe4fdaf0b102c6430303d159cdbb1cf4
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-03-05 22:29:30 -0800

    added red heart in weather message

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var WeatherMessage = ({location, temp}) => {
   var tempInF = Math.round(temp);
   var tempInC = Math.round((temp - 32) * (5/9));
   return (
-    <h3 className='text-center'>It's {tempInF}°F / {tempInC}°C in {location} :)</h3>
+    <h3 className='text-center'>It's {tempInF}°F / {tempInC}°C in {location} ❤️️</h3>
   )
 }
 module.exports = WeatherMessage;
